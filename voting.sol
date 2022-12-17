6fcd1afd15aeded0cbddcd0d7d7dd4aa828a06da
Gaurav-1908 <75855318+Gaurav-1908@users.noreply.github.com>
2022-12-17 20:29:06 +0530

    Update voting.sol

--- voting.sol
+++ voting.sol
@@ -1,51 +1,89 @@
 //SPDX-License-Identifier:MIT
-pragma solidity ^0.8;
+pragma solidity ^0.8.17;
 
-contract voting{
+contract ElectoralContract {
     struct Voter {
-        uint voter_id;
-        bytes32 name;
-        uint is_voted;
-
+        uint256 voterId;
+        string voterName;
+        bool hasVoted;
+        bool isLoggedIn;
+        string voterPassword;
     }
 
-    struct Party {
-       uint party_id;
-       bytes32 name;
-       int vote_count;
+    struct Candidate {
+        string candidateName;
+        uint256 voteCount;
     }
 
-    // Voter[] public voters;
-    mapping(uint => Voter) public voters;
-    Party[] public parties;
+    Voter[] private voters;
+    string[] private voterNames;
+    Candidate[] private candidates;
+    string[] private candidateNames;
 
-    constructor(bytes32[] memory party_names){
-        for(uint i = 0; i < party_names.length; i++){
-            parties.push(
-                Party(
-                    {
-                        party_id : i,
-                        name : party_names[i],
-                        vote_count : 0
+    constructor(string[] memory _candidateNames, string[] memory _voterNames, string[] memory _password) {
+        for (uint256 i = 0; i < _candidateNames.length; i++)
+            addCandidate(_candidateNames[i]);
+        for (uint256 i = 0; i < _voterNames.length; i++)
+            addVoter(_voterNames[i], _password[i]);
+    }
+
+    function addCandidate(string memory candidateName) public {
+        candidates.push(
+            Candidate({candidateName: candidateName, voteCount: 0})
+        );
+        candidateNames.push(candidateName);
+    }
+
+    function addVoter(string memory voterName, string memory voterPassword) public {
+        voters.push(
+            Voter(
+                {
+                    voterId: voters.length,
+                    voterName: voterName, 
+                    hasVoted: false,
+                    voterPassword: voterPassword,
+                    isLoggedIn : false
                     }
                 )
             );
-        }
+        voterNames.push(voterName);
     }
 
-    function vote(uint voter_id,uint party_id) public{
-        Voter memory voter = voters[voter_id];
-        require(voter.is_voted == 0);
-        for(uint i = 0; i < parties.length; i++){
-            if (parties[i].party_id == party_id) {
-                parties[i].vote_count += 1;
-                break;
-            }
-        }
+    function login(uint256 voterId,string memory _password) public returns (bool) {
+        require(!voters[voterId].isLoggedIn, "User Already Logged In");
+        require(keccak256(abi.encodePacked(voters[voterId].voterPassword)) == keccak256(abi.encodePacked(_password)),"Invalid Credentials");
+        voters[voterId].isLoggedIn == true;
+        return true;
     }
 
-    
+    function logout(uint256 voterId) public {
+        voters[voterId].isLoggedIn = false;
+    }
 
+    function getResult() public view returns (Candidate[] memory) {
+        return candidates;
+    }
 
+    function getVoters() public view returns (string[] memory) {
+        return voterNames;
+    }
 
+    function getCandidates() public view returns (string[] memory) {
+        return candidateNames;
+    }
+
+    function getVotersDetailed() public view returns (Voter[] memory) {
+        return voters;
+    }
+
+    function vote(uint256 voterId, uint256 candidateId) public {
+        require(!voters[voterId].hasVoted);
+        require(voters[voterId].isLoggedIn);
+        for (uint256 i = 0; i < candidates.length; i++) {
+            if (i != candidateId) continue;
+            candidates[i].voteCount++;
+            voters[voterId].hasVoted = true;
+            break;
+        }
+    }
 }
